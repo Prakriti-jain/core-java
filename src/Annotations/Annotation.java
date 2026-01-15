a14318b179b8f3a515e5f4be1a162260dcd256ea
Prakriti Jain <nonijain740@gmail.com>
2026-01-15 17:13:38 +0530

    added annotations

---
+++ src/Annotations/Annotation.java
@@ -0,0 +1,16 @@
+package Annotations;
+
+/*
+Annotations in Java provide metadata for the code, they don't change the execution of the
+code directly but give information to the compiler or runtime tools.
+They start with special markers @
+
+Compiler Annotations -
+@Override: Indicates a method overrides a superclass method.
+@SuppressWarnings: Suppresses compiler warnings for the annotated code.
+@Deprecated: Marks a class, method, or field as deprecated.
+ */
+
+
+public class Annotation {
+}
